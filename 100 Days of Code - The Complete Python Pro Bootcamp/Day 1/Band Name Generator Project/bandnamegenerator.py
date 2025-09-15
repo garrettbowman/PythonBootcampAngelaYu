d9dd67345682c3ceacda64b49dab75dabaf075cd
garrett <garrettbowman96@gmail.com>
2025-09-14 20:34:23 -0400

    Last part of day1 coding

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/Band Name Generator Project/bandnamegenerator.py
@@ -0,0 +1,12 @@
+# Write your code below this line 👇
+#print(len(input("what is your name?")))
+print("Welcome to the Band Name Generator")
+
+
+city = input("what city did you grow up in?\n")
+petname = input("what is your pets name?\n")
+
+length= len(city)
+
+
+print("Your band name could be " + city + " " + petname)
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Data Types/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Data Types/task.py
@@ -1,1 +1,7 @@
 len("Hello")
+
+print(type(23423))
+print(type(23423))
+print(type(23423))
+print(int("123")+int("23"))
+
