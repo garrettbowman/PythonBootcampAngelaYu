1fee8a6e2fd501bda4b1ce166ac18f0add823ae5
garrett <garrettbowman96@gmail.com>
2025-09-18 19:24:27 -0400

    day2 work

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Number Manipulation/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Number Manipulation/task.py
@@ -1,1 +1,10 @@
 bmi = 84 / 1.65 ** 2
+
+# Flooring the number by converting it into int
+print(int(bmi))
+
+# Rounding the number into a whole number
+print(round(bmi))
+
+# Rounding only to 2 decimal places
+print(round(bmi, 2))
