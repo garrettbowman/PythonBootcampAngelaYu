5fa6135926bb4f5a17fa2af19764398599f4ef54
garrett <garrettbowman96@gmail.com>
2025-09-18 21:50:06 -0400

    day 3 exercises

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Nesting and Elif/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Nesting and Elif/task.py
@@ -3,5 +3,14 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+    if age >1:
+     print("lol")
+    elif age >11:
+        print("gg")
+
+    else:
+        print("ok")
+    #etc..
+
 else:
     print("Sorry you have to grow taller before you can ride.")
