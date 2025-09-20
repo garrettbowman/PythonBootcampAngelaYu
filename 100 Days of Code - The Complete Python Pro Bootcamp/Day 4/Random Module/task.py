80fee77a99c7ade977f869831edbe2e0ee1f61c6
garrett <garrettbowman96@gmail.com>
2025-09-20 10:13:07 -0400

    heads tails game

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 4/Random Module/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 4/Random Module/task.py
@@ -1,4 +1,22 @@
 import random
 
-randominteger = random.randint(2, 10)
+randominteger = random.randint(0, 1)
 print(randominteger)
+
+randomfloat = random.uniform(1,10)
+print(randomfloat)
+
+userinput = input("heads or tails")
+
+if randominteger == 1:
+    if userinput == "heads":
+        
+            print("heads, you win")
+    else:
+        print("heads, you lose")
+
+else:
+    if userinput == "tails":
+        print("tails, you win")
+    else:
+        print("tails, you lose")
