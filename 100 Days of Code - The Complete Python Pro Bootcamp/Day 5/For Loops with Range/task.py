026a726c1761226bfc2926355b4e52cd8421d048
garrett <garrettbowman96@gmail.com>
2025-09-21 08:43:11 -0400

    password generator finishing day 5

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/For Loops with Range/task.py
@@ -0,0 +1,9 @@
+for number in range(1, 11, 3):
+    print(number)
+
+finalnumber = 0
+for number in range(0, 101):
+    finalnumber += number
+    
+    print(number)
+print(finalnumber)
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Password Generator Project/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,22 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+length = nr_letters + nr_symbols + nr_numbers
+password = ""
+for char in range(0, length):
+    
+    nextchar = random.choice(["nletters", "nnumbers", "nsymbols"])
+    if nextchar == "nletters":
+        if nr_letters != 0:
+            password += random.choice(letters)
+            nr_letters -= 1
+    elif nextchar == "nnumbers":
+        if nr_numbers != 0:
+            password += random.choice(numbers)
+            nr_numbers -= 1
+    elif nextchar == "nsymbols":
+        if nr_symbols != 0:
+            password += random.choice(symbols)
+            nr_symbols -= 1
 
+print(password)
