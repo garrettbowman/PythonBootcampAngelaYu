60120a074f978e59f7a168049d0ec2d0532a3b41
garrett <garrettbowman96@gmail.com>
2025-09-26 04:05:01 -0400

    added def to robot functions so vscode stopped yelling at me

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 6/Functions/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 6/Functions/task.py
@@ -1,8 +1,20 @@
-def turn_right()
+def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
+def front_is_clear():
+    print("ok bud")
+def move():
+    print("ok bud")
+def at_goal():
+    print("ok bud")
+def wall_in_front():
+    print("ok bud")
+def turn_left():
+    print("ok bud")
+def wall_on_right():
+    print("ok bud")
 def jump():
     turn_left()
 
