f44414fd6639fd90f23e1dcdd8cc383449163d51
garrett <garrettbowman96@gmail.com>
2025-09-27 12:06:19 -0400

    Finished hangman exercise

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 7/task/main1.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 7/task/main1.py
@@ -1,12 +1,15 @@
 import random
+
+
+from hangmanwordsandart import word_list, HANGMANPICS, logo
 # NO COMMENTS VERSION
 
+state = 0
 game_over = False
-
-word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
+print(logo)
 placeholder = ""
 for char in range(len(chosen_word)):
     placeholder += "_"
@@ -15,32 +18,40 @@ print(placeholder)
 stored_guess = ""
 
 while game_over == False:
-
+    print (HANGMANPICS[state])
     guess = input("Guess a letter: ").lower()
     print(guess)
-
+    if guess.isalpha() == False or len(guess) != 1:
+        print("You can only guess one letter at a time")
+    if guess in stored_guess:
+        print("You've already guessed that letter")
+    if guess not in chosen_word:
+        state += 1
+        if state == 6:
+            print (HANGMANPICS[state])
+            game_over = True
+            print(f"You lose. The word was {chosen_word}")
+            break
     stored_guess += guess
 
-    for char in chosen_word:
-        if char == guess:
-            print("Right")
-        else:
-            print("Wrong")
+    # for char in chosen_word:
+    #     if char == guess:
+    #         print("Right")
+    #     else:
+    #         print("Wrong")
 
-
-
-    display = ""
+    # display = ""
     
     for char in range(len(chosen_word)):
         
         for letter in stored_guess:
             
             if chosen_word[char] == letter: 
-                display += letter
+                # display += letter
                 if placeholder[char] == "_":
                     placeholder= placeholder[:char] + letter + placeholder[char+1:]
                     if placeholder == chosen_word:
                         game_over = True
+                        print("You win")
     print(placeholder)
 
-print("You win")
