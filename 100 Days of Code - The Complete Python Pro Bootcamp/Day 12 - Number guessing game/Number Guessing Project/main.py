33cd38dbb26fea853354ed82cfa0f40dcf1a638b
garrett <garrettbowman96@gmail.com>
2025-10-04 01:06:03 -0400

    finished my version of number guessing. could be better but moving on.

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 12 - Number guessing game/Number Guessing Project/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 12 - Number guessing game/Number Guessing Project/main.py
@@ -2,30 +2,79 @@ import random
 
 from art import logo
 
-print(logo)
+EASY_TURNS = 10
+HARD_TURNS = 5
 
-continueplaying = True
-def calculate(guess, num):
-    print(f"You are { guess } off")
+def calculate(guess, num, i):
+    '''Not the greatest organized calculation'''
+    global correct, guesses, guessesleft, wrong
+    if int(guess) == int(num):
+        correct = True
+        return
+    
+    guessesleft -= 1
+    if guessesleft == 0:
+        print("out of guesses")
+        wrong = True
+        return
+    
+    if int(num) - int(guess) > 0:
+        print("Too low\nguess again")
+    else:
+        print("Too high\nguess again")
+    print(f"You have { guesses - i -1 } guesses left")
+    print(f"You are { int(num) - int(guess) } off\n")
 
-while continueplaying:
-    guessme = random.randint(1,100)
+def set_difficulty():
     mode = input("easy or hard mode?:")
     if mode == "hard":
-        guesses = 5
+        return HARD_TURNS
     else:
-        guesses = 10
-    
-    for i in range(0,guesses):
-        if i == 0:
-            userguess = input("what is your first guess?")
-            calculate(userguess,guessme)
+        return EASY_TURNS
+
+# def correct():
+
+
+continueplaying = True
+
+
+while continueplaying:
+    correct = False
+    wrong = False
+    print(logo)
+
+    guessme = random.randint(1,100)
+    guesses = set_difficulty()
+    guessesleft = guesses
+
+    for i in range(0,guesses+1):
+        if correct == True:
+            print("You got it")
+            playagain = input("Play again? y or n: ")
+            if playagain == "y":
+                break
+            else:
+                continueplaying = False
+                print("Goodbye")
+                break
+        elif wrong == True:
+            print("You lose mate\n")
+            playagain = input("Play again? y or n: ")
+            if playagain == "y":
+                break
+            else:
+                continueplaying = False
+                print("Goodbye")
+                break
+        elif i == 0:
+            userguess = input("what is your first guess? \n")
+            calculate(userguess,guessme,i)
         elif i == 1:
-            userguess = input("what is your 2nd guess?")
-            calculate(userguess,guessme)
+            userguess = input("what is your 2nd guess? \n")
+            calculate(userguess,guessme,i)
         elif i == 2:
-            userguess = input("what is your 3rd guess?")
-            calculate(userguess,guessme)
+            userguess = input("what is your 3rd guess? \n")
+            calculate(userguess,guessme,i)
         else:
-            userguess = input(f"what is your {i+1}th guess?")
-            calculate(userguess,guessme)
+            userguess = input(f"what is your {i+1}th guess? \n")
+            calculate(userguess,guessme,i)
