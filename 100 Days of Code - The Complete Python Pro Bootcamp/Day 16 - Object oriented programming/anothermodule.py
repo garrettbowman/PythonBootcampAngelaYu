d601eece94d4171c11619496bd1ca11042b916d6
garrett <garrettbowman96@gmail.com>
2025-10-07 13:38:32 -0400

    switching to pycharm for ui reasons

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16 - Object oriented programming/anothermodule.py
@@ -0,0 +1,3 @@
+def Turtle():
+
+    return
---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16 - Object oriented programming/main.py
@@ -0,0 +1,15 @@
+import anothermodule
+import anothermodule
+import turtle
+
+from turtle import Turtle, Screen
+
+gary = Turtle()
+gary.shape("turtle")
+gary.color("coral")
+myscreen = Screen()
+gary.forward(100)
+print(myscreen.canvheight)
+print (gary)
+
+myscreen.exitonclick()
