e8f0cb4c91a6c5240c03e2b4acf863a2cff4b01f
garrett <garrettbowman96@gmail.com>
2025-10-23 13:46:26 -0400

    Finished password manager JSON, now working on day 31 capstone

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-30-exception-error-keyerror-indexerror/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-30-exception-error-keyerror-indexerror/main.py
@@ -3,9 +3,29 @@
 try:
     file = open("afile.txt")
     dic = {"key": "value"}
-    print(dic["erwer"])
+    print(dic["key"])
 
 except FileNotFoundError:
     file = open("afile.txt",mode="w")
-except KeyError:
-    print("That file does not exist")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
+
+else:
+    content = file.read()
+    print(content)
+
+finally:
+    # file.close()
+    # print("file was closed")
+    # raise TypeError("This is an error that i made up.")
+    pass
+
+
+height = float(input("height: "))
+weight = float(input("weight: "))
+
+
+if height > 3:
+    raise ValueError("Human height should not be over 3m")
+bmi = weight / height **2
+print(bmi)
