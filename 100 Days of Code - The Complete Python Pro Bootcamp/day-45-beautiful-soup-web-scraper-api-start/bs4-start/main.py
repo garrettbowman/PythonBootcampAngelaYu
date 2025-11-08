2bb12b45d3ddff075274c369ee901014461c88d1
garrett <garrettbowman96@gmail.com>
2025-11-08 09:42:35 -0500

    bs4 import and making changes

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-45-beautiful-soup-web-scraper-api-start/bs4-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-45-beautiful-soup-web-scraper-api-start/bs4-start/main.py
@@ -1,5 +1,36 @@
 from bs4 import BeautifulSoup
+# import lxml
 
 with open("website.html") as file:
     content = file.read()
-    print(content)
+    # print(content)
+
+soup = BeautifulSoup(content, "html.parser")
+# soup = BeautifulSoup(content, "lxml")
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
+
+all_anchor = soup.find_all("a")
+print(soup.find_all("a"))
+
+text = []
+for tag in all_anchor:
+    text.append(tag.getText())
+
+print(text)
+
+for tag in all_anchor:
+    # text.append(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find_all(name = "h1", id = "name")
+print(heading)
+print(heading[0].getText())
+
+class_heading = soup.find_all(class_ = "heading")
+# needs underscore because python
+print(class_heading)
+
+company_url = soup.select_one(selector= "p a")
+print(company_url)
