2af4852209813cab74909a12eeba1813077e546a
garrett <garrettbowman96@gmail.com>
2025-12-09 17:42:59 -0500

    day 53 progress

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-53-zillow-beautifulsoup-selenium-capstone-data-entry/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-53-zillow-beautifulsoup-selenium-capstone-data-entry/main.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+# Import exceptions
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 zillow_url = "https://appbrewery.github.io/Zillow-Clone/"
@@ -8,3 +13,5 @@ response = requests.get(url=zillow_url, headers=header)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
+houses = soup.select()
+print(houses)
