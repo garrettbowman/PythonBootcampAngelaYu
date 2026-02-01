e14f2c0a1a9a476af1ae5ced68dc580dce9851d1
Garrett <garrettbowman96@gmail.com>
2026-02-01 00:32:15 -0500

    starting day 58 bootstrap

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-57-blog-templating-flask-jinja-api/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
+
+@app.route("/post/<int:num>")
+def blog(num):
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    post = response.json()
+    return render_template("post.html", posts=post, num=num-1)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- 100 Days of Code - The Complete Python Pro Bootcamp/day-57-jinja-flask-api-template-html/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-57-jinja-flask-api-template-html/main.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template
+import random
+import datetime
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    random_n = random.randint(1, 10)
+    year = datetime.datetime.now().year
+    return render_template("index.html",num=random_n,year=year)
 
+
+@app.route('/guess/<string:name>')
+def naming(name):
+    name = name.capitalize()
+    gender1 = requests.get(f"https://api.genderize.io?name={name}")
+    gender = gender1.json()
+    age1 = requests.get(f"https://api.agify.io?name={name}")
+    age = age1.json()
+
+
+    return render_template("nameguess.html",name=name,gender=gender["gender"],age=age["age"])
+@app.route('/blog/<num>')
+def blog(num):
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    blogg = response.json()
+
+
+    return render_template("blog.html",posts=blogg)
 
 if __name__ == "__main__":
     app.run(debug=True)
